1c0061c503841ef1ef087ed878a86f5c9ea6cd99
engelgart-oleg <olegengelgart@gmail.com>
2026-02-19 15:53:25 +0500

    Коммит к усложненному заданию, урока № 7

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 "use strict";
 
+const week = [
+  "Понедельник", 
+  "Вторник", 
+  "Среда", 
+  "Четверг", 
+  "Пятница", 
+  "Суббота", 
+  "Воскресенье"
+];
+
+let todayIndex = new Date().getDay();
+
+todayIndex = (todayIndex === 0) ? 6 : todayIndex - 1;
+
+week.forEach((day, index) => {
+  let style = "";
+
+  if (index === 5 || index === 6) {
+    style += "font-style: italic; ";
+  }
+
+  if (index === todayIndex) {
+    style += "font-weight: bold; ";
+  }
+
+  console.log(`%c${day}`, style);
+});
